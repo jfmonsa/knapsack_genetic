cd66ba7cbe3b7ca2916b45dd659db165432e3402
jfmonsa <juanmonsalve@protonmail.com>
2024-04-25 09:31:48 -0500

    DP algo finished, not working

--- dynamic_prog.py
+++ dynamic_prog.py
@@ -16,12 +16,40 @@ def knapsack(weights: List[int], values: List[int], capacity: int) -> int:
 
     # Number of items
     n: int = len(weights)
+    # Creating DP matrix
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
-    return
+    # Iterate through each item
+    for i in range(1, n + 1):
+        # Iterate through each capacity
+        for w in range(1, capacity + 1):
+            # If the weight of the current item is less than or equal to the capacity
+            if weights[i - 1] <= w:
+                # We have two choices:
+                # 1. Take the current item and add its value to the value of the knapsack with reduced capacity
+                # 2. Do not take the current item
+                dp[i][w] = max(
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]
+                )
+
+            else:
+                # If the weight of the current item is greater than the capacity,
+                # we cannot take it, so we just use the value of the knapsack without the current item
+                dp[i][w] = dp[i - 1][w]
+
+    # The maximum value will be stored in the bottom-right cell of the table
+    return dp[n][capacity]
+
 
 # Driver Code
-if __name__ == '__main__':
-    # profit = [60, 100, 120]
-    # weight = [10, 20, 30]
-    # W = 50
-    # print knapsack(W, weight, profit)
+if __name__ == "__main__":
+    # expmaple 1
+    weights = [60, 100, 120]
+    values = [10, 20, 30]
+    capacity = 5
+    print("Maximum value 1:", knapsack(weights, values, capacity))
+
+    weights = [2, 3, 4, 5]
+    values = [3, 4, 5, 6]
+    capacity = 5
+    print("Maximum value 2:", knapsack(weights, values, capacity))
