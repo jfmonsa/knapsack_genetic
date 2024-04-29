e2f20a7097d55b9cc2da3638af30e957636791ca
DanilsGit <danielestebanmarquez12@gmail.com>
2024-04-29 16:17:50 -0500

    Add graphics

--- graphics2.py
+++ graphics2.py
@@ -1,7 +1,10 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 import random
 from typing import List
 
-# Función de aptitud
+
 def fitness(individual: List[int], weights: List[int], values: List[int], capacity: int) -> int:
     total_value = 0
     total_weight = 0
@@ -40,12 +43,7 @@ def genetic_algorithm(weights: List[int], values: List[int], capacity: int, popu
     population = []
     for _ in range(population_size):
         individual = [random.randint(0, 1) for _ in range(len(weights))]
-        #ejemplo de individuo: 
-        #donde la mochila es de espacio 11
-        #[1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
         population.append(individual)
-        #un conjunto de soluciones es una población
-        #una solución es un individuo
 
     for _ in range(num_generations):
         population = selection(population, weights, values, capacity)
@@ -61,9 +59,6 @@ def genetic_algorithm(weights: List[int], values: List[int], capacity: int, popu
     best_individual = max(population, key=lambda individual: fitness(individual, weights, values, capacity))
     return fitness(best_individual, weights, values, capacity)
 
-
-import time
-
 def measure_execution_time(examples: List[dict]) -> List[float]:
     execution_times = []
     for example in examples:
@@ -75,12 +70,72 @@ def measure_execution_time(examples: List[dict]) -> List[float]:
         mutation_rate = example['mutation_rate']
         
         start_time = time.time()
-        total_value = genetic_algorithm(weights, values, capacity, population_size, num_generations, mutation_rate)
+        genetic_algorithm(weights, values, capacity, population_size, num_generations, mutation_rate)
         end_time = time.time()
         
         execution_times.append((end_time - start_time) * 1000)
-    return [execution_times, total_value]
+    return execution_times
 
+# Dynamic programming solution
+def knapsack(weights: List[int], values: List[int], capacity: int) -> int:
+    """
+    A function to solve the 0/1 knapsack problem using dynamic programming.
+
+    Parameters:
+    + weights (List[int]): List of weights of items.
+    + values (List[int]): List of values of items.
+    + capacity (int): Maximum capacity of the knapsack.
+
+    Returns:
+    + int: Maximum value that can be obtained.
+    """
+
+    # Number of items
+    n = len(weights)
+    # Creating DP matrix
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    # Iterate through each item
+    for i in range(1, n + 1):
+        # Iterate through each capacity
+        for w in range(1, capacity + 1):
+            # If the weight of the current item is less than or equal to the capacity
+            if weights[i - 1] <= w:
+                # We have two choices:
+                # 1. Take the current item and add its value to the value of the knapsack with reduced capacity
+                # 2. Do not take the current item
+                # -> We chose the option that maximizes the total output
+                dp[i][w] = max(
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]
+                )
+
+            else:
+                # If the weight of the current item is greater than the capacity,
+                # we cannot take it, so we just use the value of the knapsack without the current item
+                dp[i][w] = dp[i - 1][w]
+
+    # The maximum value will be stored in the bottom-right cell of the table
+    return dp[n][capacity]
+
+
+def measure_execution_time_knapsack(examples: List[dict]) -> List[float]:
+    execution_times = []
+    for example in examples:
+        weights = example['weights']
+        values = example['values']
+        capacity = example['capacity']
+        
+        start_time = time.time()
+        knapsack(weights, values, capacity)
+        end_time = time.time()
+        
+        execution_times.append((end_time - start_time) * 1000)
+    return execution_times
+
+capacity = 50
+population_size = 20
+num_generations = 10
+mutation_rate = 0.05
 
 
 def generate_sequence(start, end, step):
@@ -95,16 +150,30 @@ def generate_sequence(start, end, step):
 
 sequence = generate_sequence(100, 1000, 100)
 
-capacity = 50
-population_size = 20
-num_generations = 10
-mutation_rate = 0.05
+examplesDina =  [
+    {'weights': sequence[i][0], 'values': sequence[i][1], 'capacity': capacity} for i in range(len(sequence))
+]
+execution_timesDina = measure_execution_time_knapsack(examplesDina)
+# print(execution_timesDina)
 
 examplesEvo =  [
     {'weights': sequence[i][0], 'values': sequence[i][1], 'capacity': capacity, 'population_size': population_size, 'num_generations': num_generations, 'mutation_rate': mutation_rate} for i in range(len(sequence))
 ]
 
+
+# print(len(sequence[1][0]))
+
 execution_timesEvolutivo = measure_execution_time(examplesEvo)
-print(execution_timesEvolutivo)
+# print(execution_timesEvolutivo)
 
 
+x_values = [len(sequence[i][0]) for i in range(len(sequence))]
+
+plt.plot(x_values, execution_timesDina, label='Dinámico O(n*w)')
+plt.plot(x_values, execution_timesEvolutivo, label='Evolutivo O(P*G*O(n))')
+plt.xlabel('Número de elementos para elegir')
+plt.ylabel('Tiempo de ejecución (ms)')
+plt.title('Comparación de tiempos de ejecución')
+plt.grid(True)
+plt.legend()
+plt.show()
